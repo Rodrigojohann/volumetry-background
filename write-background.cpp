6c5b0f90444f24291f7cf3626bfb8a6247737b78
rodrigo <rodrigo@debian.debian>
2021-08-03 15:19:53 -0300

    code cleaned

--- write-background.cpp
+++ write-background.cpp
@@ -11,39 +11,9 @@
 #include "PointXYZ.h"
 #include "PointCloudPlyWriter.h"
 #include "VisionaryAutoIPScan.h"
-#include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/surface/convex_hull.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl/common/common.h>
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PassThrough<pcl::PointXYZ> passx;
-pcl::PassThrough<pcl::PointXYZ> passy;
-pcl::PassThrough<pcl::PointXYZ> passz;
-pcl::ConvexHull<pcl::PointXYZ> chull;
-pcl::PointCloud<pcl::PointXYZ>::Ptr surface_hull(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-pcl::PointXYZ minPt, maxPt;
-std::vector<pcl::Vertices> polygons;
-double volume;
-double volumemean;
 std::vector<PointXYZ> pointCloud;
 boost::shared_ptr<VisionaryTData> pDataHandler;
-int counter;
-int characters_buffered;
-bool pressed;
-struct termios original;
-struct termios term;
-size_t cloud_size;
-double dimensionX;
-double dimensionY;
-double dimensionZ;
-
 ///////////////////////////////////////////////////////
 
 void runStreamingDemo(char* ipAddress, unsigned short port)
