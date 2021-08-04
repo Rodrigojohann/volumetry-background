b8a91fb6a3822a0daeb4189cefa23e07a6d948bd
rodrigo <rodrigo@debian.debian>
2021-08-04 15:10:49 -0300

    cloud concat added

--- write-background.cpp
+++ write-background.cpp
@@ -12,8 +12,12 @@
 #include "PointCloudPlyWriter.h"
 #include "VisionaryAutoIPScan.h"
 
+
+
 std::vector<PointXYZ> pointCloud;
 boost::shared_ptr<VisionaryTData> pDataHandler;
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_concat (new pcl::PointCloud<pcl::PointXYZ>);
 ///////////////////////////////////////////////////////
 
 void runStreamingDemo(char* ipAddress, unsigned short port)
@@ -37,15 +41,29 @@ void runStreamingDemo(char* ipAddress, unsigned short port)
 	control.stopAcquisition();
 	control.startAcquisition();
 	
+	for (int i = 0; i < 10; i++)
+	{
 	if (dataStream.getNextFrame())
 		{
 			// Convert data to a point cloud
 			pDataHandler->generatePointCloud(pointCloud);
 		}
 
+	cloud->points.resize (pointCloud.size());
+
+	for(size_t i=0;i<cloud->points.size();++i)
+	{
+		cloud->points[i].x = pointCloud[i].x;
+		cloud->points[i].y = pointCloud[i].y;
+		cloud->points[i].z = pointCloud[i].z;
+	}
+	
+	cloud_concat += cloud;
+	}
+	
 	char* plyFilePath = "backgroundcloud.ply";
 	printf("Writing frame to %s\n", plyFilePath);
-	PointCloudPlyWriter::WriteFormatPLY(plyFilePath, pointCloud, true);
+	PointCloudPlyWriter::WriteFormatPLY(plyFilePath, cloud_concat, true);
 	printf("Finished writing frame to %s\n", plyFilePath);
 	
 	
